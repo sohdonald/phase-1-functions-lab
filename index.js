a9fcccd8be677b479656a5b627c3cf45dc4b75af
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-04-01 01:50:17 -0400

    like a boss

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution in this file!
+/* function distanceFromHqInBlocks() {
+    const start = 42;
+    const finish = 50;
+    return finish - start;
+}
+
+function distanceFromHqInFeet(start, finish) { 
+    const feetDistance = (finish-start)*264;
+    return feetDistance;
+
+These functions will output negative numbers if the value of finish is
+less than start
+
+} */
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+// we need Math.abs() to ensure a positive number
+
+function distanceFromHqInFeet(someValue) {
+     
+    return distanceFromHqInBlocks(someValue) * 264;
+
+}
+
+// like a boss. 6 passed, 7 left 
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(destination - start)) * 264;
+}
+
+//9 down
+
+function calculatesFarePrice(start, destination) {
+
+    if (distanceTravelledInFeet(start,destination) <= 400) {
+        return 0;
+    }
+
+    else if (distanceTravelledInFeet(start,destination) > 400 && 
+    distanceTravelledInFeet(start,destination) <= 2000) {
+
+        return (distanceTravelledInFeet(start,destination) - 400) * .02;
+    }
+
+    else if (distanceTravelledInFeet(start,destination) > 2000 &&
+    distanceTravelledInFeet(start,destination) <= 2500) {
+
+        return 25;
+    }
+
+    else {
+
+        return 'cannot travel that far'
+    }
+}
